685a62b6fbb7431ac19acd10608ed845291fee15
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-06-02 22:31:10 -0400

    Organize JUnits into Suites

---
+++ junit-basic/src/test/java/com/example/junitbasic/helper/AllTests.java
@@ -0,0 +1,11 @@
+package com.example.junitbasic.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({QuickBeforeAfterTest.class, StringHelperTest.class})
+public class AllTests {
+
+}
