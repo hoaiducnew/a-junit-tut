eeb62e67713e4c0ea2ecd3a31a6b53e0b2aad3c8
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-07-12 12:29:12 -0400

    Verify calls on Mocks

--- mockito-tut01/src/test/java/com/example/mockitotut01/data/stub/business/TodoBusinessImplMockTest.java
+++ mockito-tut01/src/test/java/com/example/mockitotut01/data/stub/business/TodoBusinessImplMockTest.java
@@ -4,7 +4,12 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
@@ -35,13 +40,41 @@ public class TodoBusinessImplMockTest {
 
     TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
     
-    // Given 
+    // given 
     given(todoServiceMock.retrieveTodos("Duke")).willReturn(todos);
     
-    // When
+    // when
     List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Duke");
     
-    // Then
+    // then
     assertThat(filteredTodos.size(), is(2));
+  }
+  
+  @Test
+  public void testDeleteTodosNotRelatedToSpring_usingBDD() {
+    TodoService todoServiceMock = mock(TodoService.class);
+
+    List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+
+    TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+    // given
+    given(todoServiceMock.retrieveTodos("Duke")).willReturn(todos);
+
+    // when
+    todoBusinessImpl.deleteTodosNotRelatedToSpring("Duke");
+
+    // then
+    verify(todoServiceMock).deleteTodo("Learn to Dance");
+
+    verify(todoServiceMock, never()).deleteTodo("Learn Spring MVC");
+
+    verify(todoServiceMock, never()).deleteTodo("Learn Spring");
+
+    verify(todoServiceMock, times(1)).deleteTodo("Learn to Dance");
+    
+    verify(todoServiceMock, atLeastOnce()).deleteTodo("Learn to Dance");
+    
+    verify(todoServiceMock, atLeast(1)).deleteTodo("Learn to Dance");
   }
 }
